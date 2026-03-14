638cc0869103eed2d423617b9ddb4856e2874ae4
nour-lx <139453481+nour-lx@users.noreply.github.com>
2026-03-15 00:56:51 +0300

    Add files via upload

---
+++ JobApp/src/main/java/com/noor/JobApp/model/JobPost.java
@@ -0,0 +1,19 @@
+package com.noor.JobApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class JobPost {
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
+}
